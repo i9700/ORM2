d31aaa72aa36fa1e0fc3b22d932e0add0726f7a0
tutao <542366504@qq.com>
2022-05-03 23:30:22 +0800

    ORM关联添加

--- student/models.py
+++ student/models.py
@@ -5,7 +5,45 @@ from django.db import models
 class Clas(models.Model):
     name = models.CharField(max_length=32, verbose_name="班级名称")
 
-class  Course(models.Model):
+    class Meta:
+        db_table = "db_class"
 
+
+class Course(models.Model):
     title = models.CharField(max_length=32, verbose_name="课程名称")
-    
+
+    # students = models.ManyToManyField("Student", db_table="db_student2course")
+    class Meta:
+        db_table = "db_course"
+
+
+class Student(models.Model):
+    SEX_CHOICES = (
+        (0, "女"),
+        (1, "男"),
+        (2, "保密"),
+    )
+    name = models.CharField(max_length=32, unique=True, verbose_name="姓名")
+    age = models.SmallIntegerField(default=18, verbose_name="年龄")
+    sex = models.SmallIntegerField(choices=SEX_CHOICES)
+    birthday = models.DateField()
+
+    # 一对多的关系: 在数据库创建一个关联字段：clas_id(在clas字段后缀新增_id)
+    clas = models.ForeignKey(to="Clas", on_delete=models.CASCADE, db_constraint=False)
+
+    # 多对多的关系: 创建第三张关系表
+    courses = models.ManyToManyField("Course", db_table="db_student2course")
+
+    # 一对一的关系: 建立关联字段,在数据库中生成关联字段: stu_detail_id
+    stu_detail = models.OneToOneField("StudentDetail", on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "db_student"
+
+
+class StudentDetail(models.Model):
+    tel = models.CharField(max_length=11)
+    addr = models.CharField(max_length=32)
+
+    class Meta:
+        db_table = "db_stu_detail"
--- student/views.py
+++ student/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Student, StudentDetail, Course, Clas
+
 
 # Create your views here.
+
+def add_student(request):
+    # 添加记录
+    # 一对多与一对一的关联属性
+    stu = Student.objects.create(name="张三", age=22, sex=1, birthday="1992-12-12", clas_id=5, stu_detail_id=1)
+    print(stu.name)
+    return HttpResponse("添加成功")
